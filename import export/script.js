c08a138ee8bb49f800d4056c5b12476cf7041e83
manprajapatie <manprajapatie@gmail.com>
2025-10-31 21:29:23 +0530

    first commit

---
+++ import export/script.js
@@ -0,0 +1,11 @@
+const a = "man"
+module.exports = a
+
+//module.exports with this we can export values
+//it only export 1 value at a time, if we want to export more so we have to write it in curly braces Example -
+
+/*
+const a = "man"
+const b = "man"
+module.exports = {first : a, second : b}
+ */
