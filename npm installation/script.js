225053522c5a6df31dec8e532da9545333655a30
manprajapatie <manprajapatie@gmail.com>
2025-11-02 09:04:17 +0530

    add package

--- npm installation/script.js
+++ npm installation/script.js
@@ -1,4 +1,23 @@
-var oneLinerJoke = require('one-liner-joke');
 
-var getRandomJoke = oneLinerJoke.getRandomJoke();
-console.log(getRandomJoke)
+// TO get random joke
+
+// var oneLinerJoke = require('one-liner-joke');
+
+// var getRandomJoke = oneLinerJoke.getRandomJoke();
+// console.log(getRandomJoke)
+
+//to create text in console
+
+const figlet = require('figlet')
+
+figlet("--*", function(err, data){
+    if(err){
+        console.log(err);
+      
+    }
+    else{
+        console.log(data);
+        
+    }
+}
+)
