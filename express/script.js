f7cef4fbba3848e31aaa4c140d82150df463badd
manprajapatie <manprajapatie@gmail.com>
2025-11-06 22:01:00 +0530

    add use (it's a middleware)

--- express/script.js
+++ express/script.js
@@ -2,9 +2,15 @@ const express = require('express')
 
 const app = express()
 
+app.use((req, res, next)=>{
+    console.log("This is middleware");
+    next()    //in middleware we stuck in same function if we don't write next() it will help us to move to another function or route
+
+})
+
 app.get(
     '/', function(req, res){
-        res.send("Hello World!")
+        res.send("Hello he he he kyu kyu he!")
     }
 )
 
