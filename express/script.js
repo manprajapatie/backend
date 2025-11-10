2812edced6d0a61af104cdc8f2f498ab2412d793
manprajapatie <manprajapatie@gmail.com>
2025-11-10 08:02:12 +0530

    remove extra file and update current file

--- express/script.js
+++ express/script.js
@@ -1,17 +1,20 @@
 const express = require('express')
-
 const app = express()
 
-app.use((req, res, next)=>{
-    console.log("This is middleware");
-    next()    //in middleware we stuck in same function if we don't write next() it will help us to move to another function or route
+//This is a middleware that is always run before any route
+//It is always take an function that hold req, res, and next
+//req - req hold all user data (that should be location or other thinks as well we can check it through console.log(req))
+//res - res hold response 
+//next() - next is used to push code one line to another because when we use middleware it stuck in it and don't go to another line, that's why we have to use next() it help code to go to another line.
+
+app.use(function(req, res, next){
+    console.log(req);
 
 })
 
-app.get(
-    '/', function(req, res){
-        res.send("Hello he he he kyu kyu he!")
-    }
-)
+app.get('/', function(req, res){
+    res.send("Hellow world")
+    
+})
 
 app.listen(3000)
